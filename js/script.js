16f5fc3befd941aad8e0397cdfadaba49d4a889f
martaszymborska <maszymborska@gmail.com>
2021-05-06 21:59:32 +0200

    first interactions added

--- js/script.js
+++ js/script.js
@@ -3,5 +3,28 @@
     const tasks = [
     ];
 
-    
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        }
+        document.querySelector(".js-taskList").innerHTML = htmlString;
+    };
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").nodeValue.trim();
+if (newTaskContent === "") {
+    return;
+}
+    });
+};
+init();
 }
