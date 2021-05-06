f6b13114f852385043aca7304ca72403009faed8
martaszymborska <maszymborska@gmail.com>
2021-05-06 20:52:52 +0200

    new beginning

--- js/script.js
+++ js/script.js
@@ -1,52 +1,7 @@
 {
 
     const tasks = [
-    const init = () => {
-        {
-            content: "mlml",
-                done: false,
-     };
-        {
-            content: "mmlm",
-                done: false,
-    };
-        {
-            content: "",
-                done: false,
-    };
     ];
-    const render = () => {
-        let htmlString = "";
-        for (const task of tasks) {
-            htmlString += `
-    <li>
-    ${task.content}
-    </li>
-    `;
-        }
-    }
-    document.querySelector(".js-TasksList").innerHTML = htmlString;
-};
-const init = () => {
-    render();
-};
-init();
-}
 
-const form = document.querySelector(".js-form");
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const newTaskContent = document.querySelector(".js-newTask").nodeValue.trim();
-    if (newTaskContent === "") {
-        return;
-    }
-    tasks.push({
-        content: newTaskContent,
-    });
-    render();
-};
-};
-
-init();
+    
 }
