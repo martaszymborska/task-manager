a1ac5d323cc159d9328999579c0d66ea1a98660e
martaszymborska <maszymborska@gmail.com>
2021-05-07 21:16:07 +0200

    adding and removing tasks

--- js/script.js
+++ js/script.js
@@ -3,38 +3,58 @@
     const tasks = [
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
             htmlString += `
             <li>
+            <button class="js-tick"></button>
+            <button class="js-remove"></button>
             ${task.content}
             </li>
             `;
         }
         document.querySelector(".js-tasksList").innerHTML = htmlString;
+    
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+    removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+            removeTask(index);
+        });
+    });
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-    }
-    const init = () => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        };
 
+        addNewTask(newTaskContent);
+    };
+
+    const init = () => {
+        render();
 
         const form = document.querySelector(".js-form");
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            if (newTaskContent === "") {
-                return;
-            };
 
-            addNewTask(newTaskContent);
-           
-            render();
-        });
+        form.addEventListener("submit", onFormSubmit);
+
     };
-    init();
+
+init();
 }
