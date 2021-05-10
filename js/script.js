32b444fe35bce9ae98273ddc27fe28b7d5f0cef6
martaszymborska <maszymborska@gmail.com>
2021-05-10 16:46:09 +0200

    bind event funcion added

--- js/script.js
+++ js/script.js
@@ -10,43 +10,61 @@
         render();
     };
 
-    const removeTask = (index) => {
-        tasks.splice(index, 1);
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
         render();
-    };
-
-    const render = () => {
-        let htmlString = "";
-        for (const task of tasks) {
-            htmlString += `
-            <li
-            ${task.done ? " style=\"text-decoration: line-through\"":""}
-            >
-            <button class="js-tick"></button>
-            <button class="js-remove"></button>
-            ${task.content}
-            </li>
-            `;
-        }
-        document.querySelector(".js-tasksList").innerHTML = htmlString;
+    }
 
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !task[taskIndex].done;
+        render();
+    }
+
+
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
             });
         });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+            <li
+            ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+            >
+            <button class="js-done">done</button>
+            <button class="js-remove">remove</button>
+            ${task.content}
+            </li>
+            `;
+        }
+    
+    document.querySelector(".js-tasksList").innerHTML = htmlString;
+
+    bindEvents();
     };
 
-    
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
         if (newTaskContent === "") {
             return;
-        };
+        }
 
         addNewTask(newTaskContent);
     };
